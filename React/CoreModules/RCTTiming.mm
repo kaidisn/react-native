3b0b2e3d6d7972606ae446f89c928a0bebc660e5
Atlante45 <clement.brisset@gmail.com>
2020-11-18 20:54:34 -0800

    Fix timers on proximity state change

--- React/CoreModules/RCTTiming.mm
+++ React/CoreModules/RCTTiming.mm
@@ -146,6 +146,11 @@ RCT_EXPORT_MODULE()
                                                  name:name
                                                object:nil];
   }
+
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(deviceProximityStateDidChange)
+                                               name:UIDeviceProximityStateDidChangeNotification
+                                             object:nil];
 }
 
 - (void)dealloc
@@ -180,6 +185,15 @@ RCT_EXPORT_MODULE()
 {
   _inBackground = NO;
   [self startTimers];
+}
+
+- (void)deviceProximityStateDidChange
+{
+  if ([[UIDevice currentDevice] proximityState]) {
+    [self appDidMoveToBackground];
+  } else {
+    [self appDidMoveToForeground];
+  }
 }
 
 - (void)stopTimers
